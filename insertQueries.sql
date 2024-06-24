3ce766317678bdc056229ed6db50346b80b80b8c
Manju Kaushik <198709km@gmail.com>
2024-06-24 17:05:49 +0530

    Update insertQueries.sql

--- insertQueries.sql
+++ insertQueries.sql
@@ -1,2 +1,1 @@
-INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-122', 122);
-INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-144', 144);
+INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-150', 150);
