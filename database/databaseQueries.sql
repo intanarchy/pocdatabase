81380c2219801e0570d199214152e1e44b671ac8
Manju Kaushik <198709km@gmail.com>
2024-06-25 20:53:38 +0530

    Create databaseQueries.sql

---
+++ database/databaseQueries.sql
@@ -0,0 +1,5 @@
+INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-158', 158);
+INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-159', 159);
+INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-160', 160);
+INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-161', 161);
+INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-162', 162);
