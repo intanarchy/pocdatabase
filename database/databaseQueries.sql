6d9ff8fc50f2b0aa23d4eebcd4de743e1c4314fb
Manju Kaushik <198709km@gmail.com>
2024-06-25 21:17:37 +0530

    Update databaseQueries.sql

--- database/databaseQueries.sql
+++ database/databaseQueries.sql
@@ -1,5 +1,1 @@
-INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-158', 158);
-INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-159', 159);
-INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-160', 160);
-INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-161', 161);
-INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-162', 162);
+INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-163', 163);
