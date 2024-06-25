795236b3dc694abff5760f2ad7ae513401cc2a40
Manju Kaushik <198709km@gmail.com>
2024-06-25 20:53:19 +0530

    Delete database directory

--- database/insert/insertQueries.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-158', 158);
-INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-159', 159);
-INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-160', 160);
-INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-161', 161);
-INSERT INTO pocaws.employeetable (empname , empid) VALUES ('Alice-162', 162);
